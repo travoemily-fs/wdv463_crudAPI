148a62ef36c39dcc8dad4345fa5a7c8dacbe2716
travoemily-fs <EATravo@student.fullsail.edu>
2025-03-14 12:10:18 -0700

    updating the character schema

--- models/character.js
+++ models/character.js
@@ -4,10 +4,57 @@ const characterSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
+    trim: true,
+  },
+  aliases: {
+    type: [String],
+    default: [],
+  },
+  age: {
+    type: Number,
+    default: null,
+    min: 0,
+  },
+  gender: {
+    type: String,
+    enum: ["female", "male", "non binary", "unknown"],
+    required: true,
+    trim: true,
+  },
+  species: {
+    type: String,
+    enum: ["supe", "human"],
+    required: true,
+    trim: true,
+  },
+  abilities: {
+    type: [String],
+    default: [],
   },
   occupation: {
     type: String,
     required: true,
+    trim: true,
+  },
+  knownAllies: {
+    type: [String],
+    default: [],
+  },
+  knownEnemies: {
+    type: [String],
+    default: [],
+  },
+  status: {
+    type: String,
+    enum: ["alive", "deceased", "unknown"],
+    default: "alive",
+    trim: true,
+  },
+  threatLevel: {
+    type: String,
+    enum: ["high", "moderate", "low", "none"],
+    default: "none",
+    trim: true,
   },
   created_at: {
     type: Date,
