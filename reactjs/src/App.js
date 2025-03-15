5216151e39d9eeb61eb9cf212574087eb3f423eb
travoemily-fs <EATravo@student.fullsail.edu>
2025-03-15 12:15:53 -0700

    basic CRA build

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -7,7 +7,7 @@ function App() {
   const [error, setError] = useState(null);
   const API_BASE =
     process.env.NODE_ENV === "development"
-      ? `http://localhost:8000`
+      ? `http://localhost:8000/api/v1`
       : process.env.REACT_APP_BASE_URL;
 
   // simulates unmounting
